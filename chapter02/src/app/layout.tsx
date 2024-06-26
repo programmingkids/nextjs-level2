75913cd57be61223086bb5f94c7966607d76fa34
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-06-26 12:20:41 +0000

    fix chapter02

--- chapter02/src/app/layout.tsx
+++ chapter02/src/app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Noto_Sans_JP } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
 
-const notoSansJP400 = Noto_Sans_JP({
-  weight: "400",
-  display: "swap",
-  preload: false,
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,10 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={notoSansJP400.className} suppressHydrationWarning={true}>
-        <Header />
+      <body className={inter.className} suppressHydrationWarning={true}>
         {children}
-        <Footer />
       </body>
     </html>
   );
